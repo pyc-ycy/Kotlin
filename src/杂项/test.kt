5e5934f5fc20e5302bc334b65f1580e788f175a7
御承扬 <2923616405@qq.com>
2019-06-20 18:00:28 +0800

    Initial commit

---
+++ src/杂项/test.kt
@@ -0,0 +1,31 @@
+package 杂项
+
+//杂项
+//彭友聪
+//2019/6/1
+//20:19
+//Kotlin
+//email:2923616405@qq.com
+class Greeter(val name:String){
+    fun greet(){
+        println("Hello,$name")
+    }
+}
+
+fun sum(a:Int,b:Int): Int {
+    return a+b
+}
+
+fun test()
+{
+    println("Hello World!")
+}
+
+fun main(args:Array<String>)
+{
+    Greeter("彭友聪").greet()
+    var c:Int = 0
+    c = sum(a = 5, b = 8)
+    println(c)
+    test()
+}
